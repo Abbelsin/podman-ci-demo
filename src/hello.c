f08f0da58c130d582a879ba9c4020105f1bc0aa7
Abbelsin <abbelsin@gmail.com>
2024-11-15 15:44:20 +0100

    Add bad formatting and fix return code

--- src/hello.c
+++ src/hello.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
+#include   <stdio.h>
 
-int main()
-{
-    // Change in the code to trigger the workflow
-    printf("Hello, Podman CI!\n");
+int   main() {
+      // Change in the code to trigger the workflow
+  printf("Hello, Podman CI!\n");
     // Workflow fails sucessfully if not returning 0 :)
-    return -1;
-}
+    return 0;}
