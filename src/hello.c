33d230b8587862680ab5f3ecaead856cfd4462b2
Abbelsin <abbelsin@gmail.com>
2024-11-15 15:55:09 +0100

    Format hello.c

--- src/hello.c
+++ src/hello.c
@@ -1,7 +1,9 @@
-#include   <stdio.h>
+#include <stdio.h>
 
-int   main() {
-      // Change in the code to trigger the workflow
-  printf("Hello, Podman CI!\n");
+int main()
+{
+    // Change in the code to trigger the workflow
+    printf("Hello, Podman CI!\n");
     // Workflow fails sucessfully if not returning 0 :)
-    return 0;}
+    return 0;
+}
