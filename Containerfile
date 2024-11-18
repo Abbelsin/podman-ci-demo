e67ad805fee5f375570790465bff3b6904c9a09c
Abbelsin <abbelsin@gmail.com>
2024-11-18 15:48:27 +0000

    lets see if anything even works here

--- Containerfile
+++
@@ -1,9 +0,0 @@
-FROM gcc:latest
-
-WORKDIR /app
-
-COPY src/hello.c .
-
-RUN gcc -o hello hello.c
-
-CMD ["./hello"]
--- src/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    // Change in the code to trigger the workflow
-    printf("Hello, Podman CI!\n");
-    // Workflow fails sucessfully if not returning 0 :)
-    return 0;
-}
